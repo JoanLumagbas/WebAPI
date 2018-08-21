0f1b5373e6708e98d526ffd68cf25d330cf5b4b1
Joan M. Lumagbas <40494730+JoanLumagbas@users.noreply.github.com>
2018-08-22 01:21:34 +0800

    Add files via upload

---
+++ WebApiSampleProject/WebApiSampleProject/Controllers/StudentController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiSampleProject.Models;
+using WebApiSampleProject.Controllers;
+
+
+namespace WebApiSampleProject.Controllers
+{
+
+    public class StudentController : ApiController
+    {
+
+        IList<Student> students = new List<Student>()
+        {
+            new Student()
+            {
+               StudentId = 1, StudentName = "Joan M. Lumagbas", StudentCourse = "BSIT", StudentYear = "4"  
+            },
+            new Student()
+            {
+               StudentId = 1, StudentName = "Giesel P. Dumalagan", StudentCourse = "BEED", StudentYear = "4"  
+            },
+            new Student()
+            {
+               StudentId = 1, StudentName = "Mary Ann Y. Lumictin", StudentCourse = "BEED", StudentYear = "3"  
+            },
+        };
+        public IList<Student> GetAllStudents()
+        {
+            //Return list of all Student  
+            return students;
+        }
+        public Student GetStudentDetails(int Studentid)
+        {
+            //Return a single Student detail  
+            var student = students.FirstOrDefault(e => e.StudentId == Studentid);
+            if (student == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+            return student;
+        }  
+
+    }
+        
+}
---
+++ WebApiSampleProject/WebApiSampleProject/Models/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace WebApiSampleProject.Models
+{
+    public class Student
+    {
+        public int StudentId
+        {
+            get;
+            set;
+        }
+        public string StudentName
+        {
+            get;
+            set;
+        }
+        public string StudentCourse
+        {
+            get;
+            set;
+        }
+        public string StudentYear
+        {
+            get;
+            set;
+        }
+    }
+}
